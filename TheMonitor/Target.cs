8798827e9da95be325c65f217a182aa0a92dc60e
Craig Reynolds <wyldphyre@gmal.com>
2013-04-21 14:34:09 +0800

    Initial commit of data model ideas

---
+++ TheMonitor/Target.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheMonitor
+{
+  class Target
+  {
+    public Target(string TargetFolder)
+    {
+      this.TargetFolder = TargetFolder;
+      this.Activities = new List<MonitoredActivity>();
+    }
+
+    public string TargetFolder { get; private set; }
+    public List<MonitoredActivity> Activities { get; private set; }
+  }
+
+  class MonitoredActivity
+  {
+    public MonitoredActivity(string FileMask, ActivityType Type)
+    {
+      this.FileMask = FileMask;
+      this.Type = Type;
+    }
+
+    public string FileMask { get; private set; }
+    private ActivityType Type { get; private set; }
+  }
+
+  enum ActivityType
+  {
+    Created,
+    Deleted,
+    Changed
+  }
+}
